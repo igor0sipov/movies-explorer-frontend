8ef978282bd37c62bc67f91fd8d8c0298f9280f4
Igor Osipov <fakealien@yandex.ru>
2021-03-25 18:16:20 +0300

    feat: about me section

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,87 @@
+import "./AboutMe.css";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import { Link } from "react-router-dom";
+import studentPhotoPath from "../../images/student.jpg";
+import arrowIconPath from "../../images/portfolio__arrow.svg";
+
+const AboutMe = () => {
+  const links = [
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/",
+    },
+  ];
+
+  const portfolioLinks = [
+    {
+      name: "Cтатичный сайт",
+      url: "https://",
+    },
+    {
+      name: "Адаптивный сайт",
+      url: "https://",
+    },
+    {
+      name: "Одностраничное приложение",
+      url: "https://",
+    },
+  ];
+
+  return (
+    <section className="student sizer">
+      <SectionHeading text="Студент" />
+      <div className="student__container">
+        <img
+          className="student__photo"
+          src={studentPhotoPath}
+          alt="Фото студента"
+        />
+        <div className="student__about">
+          <h3 className="student__name">Виталий</h3>
+          <p className="student__status">Фронтенд-разработчик, 30 лет</p>
+          <p className="student__bio">
+            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
+            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
+            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Контур». После того, как прошёл курс по веб-разработке, начал
+            заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <ul className="student__links">
+            {links.map((link, index) => {
+              return (
+                <li className="student__list-element" key={index}>
+                  <a className="student__link" href={link.url}>
+                    {link.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="portfolio">
+        <h4 className="portfolio__heading">Портфолио</h4>
+        <ul className="portfolio__links">
+          {portfolioLinks.map((link, index) => {
+            return (
+              <li className="portfolio__link" key={index}>
+                <a className="portfolio__url" href={link.url}>
+                  {link.name}
+                </a>
+                <a className="portfolio__arrow" href={link.url}>
+                  {""}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,16 @@ import "./Main.css";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 const Main = () => {
   return (
-    <section className="main">
+    <main className="main">
       <Promo />
       <AboutProject />
       <Techs />
-    </section>
+      <AboutMe />
+    </main>
   );
 };
 
