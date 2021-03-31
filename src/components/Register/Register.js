195cc418b8e1c12c28bb4f900482d99ac5afa98d
Igor Osipov <fakealien@yandex.ru>
2021-03-31 21:36:38 +0300

    feat: register component

---
+++ src/components/Register/Register.js
@@ -0,0 +1,88 @@
+import Auth from "../Auth/Auth";
+import "./Register.css";
+import { useState } from "react";
+
+const Register = () => {
+  const [inputValues, setInputValues] = useState({
+    username: {
+      text: "",
+      isValid: true,
+      validationMessage: "",
+    },
+    email: {
+      text: "",
+      isValid: true,
+      validationMessage: "",
+    },
+    password: {
+      text: "",
+      isValid: true,
+      validationMessage: "",
+    },
+  });
+
+  const onInputChange = (e) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: {
+        text: e.target.value,
+        isValid: e.target.validity.valid,
+        validationMessage: e.target.validationMessage,
+      },
+    });
+  };
+
+  const inputs = [
+    {
+      name: "username",
+      type: "text",
+      label: "Имя",
+      placeholder: "Введите имя",
+      value: inputValues.username.text,
+      min: 2,
+      max: 30,
+      required: true,
+      isValid: inputValues.username.isValid,
+      validationMessage: inputValues.username.validationMessage,
+    },
+    {
+      name: "email",
+      type: "email",
+      label: "E-mail",
+      placeholder: "Введите e-mail",
+      value: inputValues.email.text,
+      min: 2,
+      max: 30,
+      required: true,
+      isValid: inputValues.email.isValid,
+      validationMessage: inputValues.email.validationMessage,
+    },
+    {
+      name: "password",
+      type: "password",
+      label: "Пароль",
+      placeholder: "Введите пароль",
+      value: inputValues.password.text,
+      min: 8,
+      max: 30,
+      required: true,
+      isValid: inputValues.password.isValid,
+      validationMessage: inputValues.password.validationMessage,
+    },
+  ];
+
+  return (
+    <main className="register">
+      <Auth
+        greeting={"Добро пожаловать!"}
+        inputs={inputs}
+        onInputChange={onInputChange}
+        question="Уже зарегестрированы?"
+        linkText="Войти"
+        direction="/signin"
+      />
+    </main>
+  );
+};
+
+export default Register;
