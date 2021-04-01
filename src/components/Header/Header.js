82e40733fa2709e614001224ecc50d73d3508d30
Igor Osipov <fakealien@yandex.ru>
2021-04-01 18:43:07 +0300

    reafactor: delete useless code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import "./Header.css";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 
 const Header = ({
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import "./PageNotFound.css";
 
